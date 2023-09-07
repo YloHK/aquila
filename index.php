99d2919c37ffed72cda49cf3ff6ebd46941a4dde
Hanniel Chow <hannielchow@Hanniels-MacBook-Pro-3.local>
2023-09-07 13:51:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila
+ */
+?>
+
+<!doctype html>
+<html lang= en>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content= "ie=edge">   
+<title>Wordpress</title>
+</head>
+<body>
+	
+</body>
+</html>
